c3f8a1cf0337576a2354326323a054431efd36de
Amanda Winkles <amandawinkles@gmail.com>
2020-03-31 18:23:45 -0500

    Complete Bare Minimum Requirements

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,29 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
+// Needs document.body, element.childNodes, and element.classList.
+var getElementsByClassName = function(className) {
   // your code here
+  // Set up an empty array holding all of the matching elements.
+  var matchingArr = [];
+  // We look at element.classList...
+  // Inner recursion function...pass in elements...
+  function checkElements(element) {
+   // Check elements...
+    // If element.classList exists, and className is in classList...push into matching elements array.
+    if(element.classList && element.classList.contains(className)) {
+      matchingArr.push(element);
+    }
+    // Iterate through element.childNodes...
+    if(element.childNodes) {
+      for(var i = 0; i < element.childNodes.length; i++) {
+        // Recursively call inner function on element.childNodes[i].
+        checkElements(element.childNodes[i]);
+      }
+    }
+  }
+  // Call inner function on document.body.
+  checkElements(document.body);
+  // Return matching elements array.
+  return matchingArr;
 };
